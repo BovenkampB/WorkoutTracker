f39e826dfbe30627b82dcce20e7439a17fe24212
bbovenkamp <bart.vandebovenkamp@deptagency.com>
2023-10-09 01:58:04 +0200

    Added first test endpoints to see if axios is reaching the endpoints and added helmet middleware so only my vuejs app can reach it

--- backendServer/app.js
+++ backendServer/app.js
@@ -1,10 +1,55 @@
 const {MongoClient, ServerApiVersion} = require("mongodb");
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
 const app = express();
 const server = require('http').Server(app);
 const port = 3000;
-const helmet = require('helmet');
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+//pre-flight requests
+app.options('*', function(req, res) {
+    res.send(200);
+});
+
+server.listen(port, (err) => {
+    if (err) {
+        throw err;
+    }
+    /* eslint-disable no-console */
+    console.log('Node Endpoints working :)');
+});
+
+app.all('/', (req, res) => {
+    if (req.method === 'GET') {
+        res.status(200);
+        res.send('working GET request');
+        res.end();
+
+    } else if (req.method === 'POST') {
+        res.status(200);
+        res.send('working POST request');
+        res.end();
+
+    } else if (req.method === 'PUT') {
+        res.status(200);
+        res.send('working PUT request');
+        res.end();
+    }
+});
+
+module.exports = server;
+
 
 
 var CONFIG = require('./config.json');
